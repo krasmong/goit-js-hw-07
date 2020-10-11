199e0880c6ed2e890cb5ccd677ca86d62724b632
Oleksandr Pomalin <nilamopr@gmail.com>
2020-10-11 18:01:35 +0300

    добавляет шестую задачу

---
+++ js/task-06.js
@@ -0,0 +1,45 @@
+// Задание 6
+// Напиши скрипт, который бы при потере фокуса на инпуте,
+// проверял его содержимое на правильное количество символов.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Введи 6 символов"
+// />
+// Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+// Если введено подходящее количество, то border инпута становится зеленым,
+// если неправильное - красным.
+// Для добавления стилей, используй CSS-классы valid и invalid.
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+
+const inputVal = document.getElementById("validation-input");
+// console.log(inputVal);
+
+inputVal.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  const value = inputVal.dataset.length;
+  if (event.currentTarget.value.length === Number(value)) {
+    inputVal.classList.add("valid");
+    inputVal.classList.remove("invalid");
+  } else {
+    inputVal.classList.add("invalid");
+    inputVal.classList.remove("valid");
+  }
+}
+
+
